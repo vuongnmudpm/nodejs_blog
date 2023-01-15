b98d04243a8eff4becda35232712021cf77dbe1d
vuongnmudpm <vuongnmudpm@gmail.com>
2023-01-15 23:20:15 +0700

    150123

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const { engine } = require ('express-handlebars');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+//HTTP Logger 
+app.use(morgan('combined'));
+//Template engine
+app.engine('hbs', engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'resources/views'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
 })
 
 //127.0.0.1
