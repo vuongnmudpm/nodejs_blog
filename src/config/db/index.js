37e3f16f43f63a1496262c1471b0aa89e6e36783
vuongnmudpm <vuongnmudpm@gmail.com>
2023-01-26 19:32:09 +0700

    vuongnm update connect MongoDB

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,1 +1,12 @@
-function connect() {}
+const mongoose = require('mongoose');
+
+async function connect() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1/f8_education_dev');
+        console.log('Connect successfully!!!');
+    } catch (error) {
+        console.log('Connect failue!!!');
+    }
+}
+
+module.exports = { connect }
